278d322aa0371da2cfb1ab1b9094d6364c899183
GreenPepperCode <max.bonnin@sfr.fr>
2023-01-21 11:21:21 +0100

    SQL fonctions : create or update characters with result; create games; update characteristics; all with test ok

--- script/Fonctions_SQL/Fonctions_characters.sql
+++ script/Fonctions_SQL/Fonctions_characters.sql
@@ -53,5 +53,32 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql
 --test de la fonction 
---SELECT get_character_by_id_with_all(1) 
+--SELECT * FROM get_character_by_id_with_all(1) 
 
+
+-- Met le perso “:id” à jour ou Créer un nouveaux perso en base de données si il n'existe pas
+CREATE OR REPLACE FUNCTION create_or_update_characters_with_result(
+	IN new_id INT,
+    IN new_firstname TEXT, 
+    IN new_lastname TEXT, 
+    IN new_description TEXT,
+    IN new_race TEXT,
+    IN new_class TEXT,
+    IN new_is_alive BOOLEAN
+
+)
+RETURNS TABLE("id" INTEGER, firstname TEXT, lastname TEXT, "description" TEXT, race TEXT, "class" TEXT, is_alive BOOLEAN, "created_at" TIMESTAMPTZ, "updated_at" TIMESTAMPTZ) AS $$
+BEGIN
+    -- En first on essai l'update 
+    UPDATE "Characters" SET firstname = new_firstname, lastname = new_lastname, "description" = new_description, race = new_race, is_alive= new_is_alive, "class" = new_class, "user_id" = new_id WHERE "new_id" = "Characters".id;
+    IF NOT FOUND THEN 
+        INSERT INTO "Characters" ( firsname, lastname, description, race, class, "user_id") VALUES ( new_firstname, new_lastname, new_description, new_race, new_class, new_user_id) RETURNING "Charaters".id INTO new_id; -- on stock la valeur de l'id créer dans "new_id"
+    END IF;
+    RETURN QUERY SELECT "Characters".id, "Characters".firstname, "Characters".lastname, "Characters".description, "Characters".race, "Characters".class, "Characters".is_alive, "Characters".created_at, "Characters".updated_at FROM "Characters" WHERE "Characters".id = new_id;
+END
+$$ LANGUAGE plpgsql;
+/*
+ Test de fonction OK
+SELECT * from create_or_update_characters_with_result(
+	2, 'grobarg', 'trackmort', 'test', 'ORKS', 'brutal badass', TRUE) 
+*/
--- script/Fonctions_SQL/Fonctions_games.sql
+++ script/Fonctions_SQL/Fonctions_games.sql
@@ -6,3 +6,28 @@ $$ LANGUAGE SQL;
 -- Test de fonction OK
 SELECT get_games()
 
+-- Créer la "Games" et retourne les info (mj=user_id)
+CREATE OR REPLACE FUNCTION create_game(IN game_name TEXT, IN nbmax_players INT, IN game_notes TEXT, IN game_status TEXT, IN gm_id INT)
+RETURNS TABLE(
+    "id" INT,
+    "name" TEXT,
+    "max_players" INT,
+    "notes" TEXT,
+    "status" TEXT,
+    "user_id" INT
+) AS $$
+--déclare un variable pour récupéré l'id de la game nouvellement créer
+DECLARE 
+    game_id INT;
+BEGIN
+    INSERT INTO "Games"("name", "max_players", "notes", "status", "user_id") 
+    VALUES (game_name, nbmax_players, game_notes, game_status, gm_id)
+    RETURNING "Games"."id" INTO game_id; --récupération de la valeur dans la variable déclaré précédemment
+    RETURN QUERY SELECT "Games"."id", "Games"."name", "Games"."max_players", "Games"."notes", "Games"."status", "Games"."user_id" FROM "Games" WHERE "Games".id = game_id;-- utilisation de la variable dans la close WHERE pour renvoyé les bonne données
+END;
+$$ LANGUAGE plpgsql;
+/*
+script de test de la fonction :
+SELECT * from create_game(
+	'newgame', 5, 'far far away', 'lvl8', 1) 
+*/
