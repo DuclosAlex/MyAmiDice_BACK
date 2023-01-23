71b76000565e82295a107555eb11d57c95eb0f78
GreenPepperCode <max.bonnin@sfr.fr>
2023-01-23 17:11:50 +0100

    user_login sql fonction with test on pairProg

--- script/Fonctions_SQL/Fonctions_connection.sql
+++ script/Fonctions_SQL/Fonctions_connection.sql
@@ -1,12 +1,16 @@
---Vérifié les infos d'un user par ses login
-CREATE OR REPLACE FUNCTION login(IN test_email email, IN test_password TEXT)
-RETURNS TABLE("id" INT, "email" email, is_admin BOOLEAN, firstname TEXT, lasname TEXT) AS $$
+--Vérifié les infos d'un user par ses login et si les info sont bonne on renvoie les perso et les games du users
+CREATE OR REPLACE FUNCTION user_login(IN test_email email, IN test_password TEXT)
+RETURNS TABLE("id" INT, "email" email, is_admin BOOLEAN, firstname TEXT, lasname TEXT, charac_name TEXT, game_name TEXT, game_status TEXT, name_user_mj TEXT, invite_status TEXT, invite_game INT) AS $$
 BEGIN
-    RETURN QUERY SELECT "Users".id, "Users".email, "Users".is_admin, "Users".firstname, "Users".lastname FROM "Users" WHERE "Users".email = test_email AND "Users".password = test_password;
+    RETURN QUERY SELECT "Users".id, "Users".email, "Users".is_admin, "Users".firstname, "Users".lastname, "Characters".firstname, "Games".name, "Games".status, "Users".pseudo, "Invite".status, "Invite".game_id FROM "Users" 
+    FULL JOIN "Games" ON "Users".id = "Games".user_id
+    FULL JOIN "Characters" ON "Users".id = "Characters".user_id
+    FULL JOIN "Invite" ON "Users".id = "Invite".user_id
+    WHERE "Users".email = test_email AND "Users".password = test_password;
 END;
 $$ LANGUAGE plpgsql;
 /*
-Test de la fonction OK
-SELECT * from login(
-	'bouya@mail.com', 'bouya')
+Test de fonction OK:
+SELECT * from login1(
+    'bouya@mail.com', 'bouya')
 */
