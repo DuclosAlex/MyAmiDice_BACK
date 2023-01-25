914f7d9742115925455ef57dc555c812943a3d97
GreenPepperCode <max.bonnin@sfr.fr>
2023-01-25 11:15:56 +0100

    sql function ajustement

--- script/Fonctions_SQL/Fonctions_connection.sql
+++ script/Fonctions_SQL/Fonctions_connection.sql
@@ -1,17 +1,14 @@
--- SQLBook: Code
---Vérifié les infos d'un user par ses login et si les info sont bonne on renvoie les perso et les games du users
 CREATE OR REPLACE FUNCTION user_login(IN test_email email, IN test_password TEXT)
-RETURNS TABLE("id" INT, "email" email, is_admin BOOLEAN, firstname TEXT, lasname TEXT, charac_name TEXT, game_name TEXT, game_status TEXT, name_user_mj TEXT, invite_status TEXT, invite_game INT) AS $$
+RETURNS TABLE("login" json) AS $$
 BEGIN
-    RETURN QUERY SELECT "Users".id, "Users".email, "Users".is_admin, "Users".firstname, "Users".lastname, "Characters".firstname, "Games".name, "Games".status, "Users".pseudo, "Invite".status, "Invite".game_id FROM "Users" 
-    FULL JOIN "Games" ON "Users".id = "Games".user_id
-    FULL JOIN "Characters" ON "Users".id = "Characters".user_id
-    FULL JOIN "Invite" ON "Users".id = "Invite".user_id
-    WHERE "Users".email = test_email AND "Users".password = test_password;
+  RETURN QUERY SELECT row_to_json("log") as "login"
+  FROM (
+    SELECT "Users".id, "Users".email, "Users".is_admin, "Users".firstname, "Users".lastname, "Users".pseudo,
+    (SELECT json_agg(row_to_json( (SELECT temptable FROM (SELECT "name", status) temptable ))) FROM "Games" WHERE "Users".id = "Games".user_id ) "games",
+    (SELECT json_agg(row_to_json( (SELECT temptable FROM (SELECT "firstname", lastname) temptable ))) FROM "Characters" WHERE "Users".id = "Characters".user_id ) "characters",
+    (SELECT json_agg(row_to_json( (SELECT temptable FROM (SELECT game_id, user_id) temptable ))) FROM "Invite" WHERE "Users".id = "Invite".user_id) "invite"
+    FROM "Users"
+    WHERE "Users".email = test_email AND "Users".password = test_password
+  ) "log";
 END;
 $$ LANGUAGE plpgsql;
-/*
-Test de fonction OK:
-SELECT * from login1(
-    'bouya@mail.com', 'bouya')
-*/
