465e1c15e631ddd59bc22f2fd93d168bc8bc1ba0
GreenPepperCode <max.bonnin@sfr.fr>
2023-01-26 10:16:14 +0100

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const { characterRouter, userRouter, newsRouter, gameRouter, itemRouter, skillRouter, mapRouter, inviteRouter, characteristicRouter } = require('./app/router');
 const multer = require('multer');
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
