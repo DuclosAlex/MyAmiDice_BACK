d43437a023b23fefee2a9e803b1f7bdeb50107f2
Duclos Alex <duclos.alex85@outlook.fr>
2023-01-30 11:38:45 +0100

    modif news

--- app/router/newsRouter.js
+++ app/router/newsRouter.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.get('/getall', newsController.basicQuery.getAll);
 router.get('/:id', newsController.basicQuery.getById);
 router.delete( '/:id', newsController.basicQuery.deleteById);
-router.post('/:id', newsController.basicQuery.createOrUpdate );
+router.post('', newsController.basicQuery.createOrUpdate );
 
 module.exports = router;
