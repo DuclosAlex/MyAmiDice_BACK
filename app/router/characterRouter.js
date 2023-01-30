46e6421a043863beeb1888a1772ff1d4e13b6c15
GreenPepperCode <max.bonnin@sfr.fr>
2023-01-30 17:47:09 +0100

    test uploadMiddleware

--- app/router/characterRouter.js
+++ app/router/characterRouter.js
@@ -1,11 +1,13 @@
+// Dans le fichier characterRouter.js
 const express = require('express');
 const { characterController } = require('../controller');
+const fileUpload = require('../services/fileUploadMiddleware')('public/characters/');
 
 const router = express.Router();
 
 router.get('/getall', characterController.basicQuery.getAll);
 router.get('/id', characterController.getCharacterByIdWithAll);
 router.delete( '/:id', characterController.basicQuery.deleteById);
-router.post('', characterController.basicQuery.createOrUpdate );
+router.post('', fileUpload.single('file'), characterController.basicQuery.createOrUpdate );
 
 module.exports = router;
---
+++ service/fileUploasMiddleware.js
@@ -0,0 +1,11 @@
+// Le fichier fileUploadMiddleware.js
+const multer = require('multer');
+
+const diskStorage = (destination) => multer.diskStorage({
+destination: (req, file, cb) => cb(null, destination),
+filename: (req, file, cb) => cb(null, file.originalname)
+});
+
+const fileUpload = (destination) => multer({ storage: diskStorage(destination) });
+
+module.exports = fileUpload;
