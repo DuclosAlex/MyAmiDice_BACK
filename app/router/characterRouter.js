da0250e3ec256790cd8ccaedd4a8186fbd3c40a8
GreenPepperCode <max.bonnin@sfr.fr>
2023-02-01 08:08:20 +0100

    file upload correction

--- app/router/characterRouter.js
+++ app/router/characterRouter.js
@@ -1,7 +1,7 @@
 // Dans le fichier characterRouter.js
 const express = require('express');
 const { characterController } = require('../controller');
-// const fileUpload = require('../../services/fileUploadMiddleware')('public/characters/');
+const fileUpload = require('../../services/fileUploadMiddleware')('public/characters/');
 
 
 const router = express.Router();
@@ -9,7 +9,7 @@ const router = express.Router();
 // router.get('/getall', characterController.basicQuery.getAll);
 router.get('/:id', characterController.getCharacterByIdWithAll);
 router.delete( '/:id', characterController.basicQuery.deleteById);
-router.post('/create', characterController.createCharacterWithCaracteristics );
-router.post('/update', characterController.basicQuery.createOrUpdate );
+router.post('/create', fileUpload.array('avatar'), characterController.createCharacterWithCaracteristics );
+router.post('/update', fileUpload.single('avatar'), characterController.updateCharacter );
 
 module.exports = router;
