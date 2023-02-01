a72e8d6163f61179cf74d521b50f20a54b36d723
Duclos Alex <duclos.alex85@outlook.fr>
2023-02-01 13:53:29 +0100

    change in user controller

--- app/controller/userController.js
+++ app/controller/userController.js
@@ -38,9 +38,14 @@ const userController = {
 
         let password = await db.query(`SELECT password FROM "Users" WHERE "Users".email = '${req.body.email} '`);
         password = password.rows[0];
+
+        console.log('password', password)
         const compare = await bcrypt.compare(req.body.password, password.password);
         req.body.password = compare;
+        console.log('compare', compare)
+        console.log('bodypassword', req.body.password)
         const user = req.body;
+        console.log('user', user)
         const result = await userModel.loginUser(user);
         if (result !== undefined) {
             const token = jwt.sign({ userId: result.user.id}, process.env.TOKEN_KEY);
