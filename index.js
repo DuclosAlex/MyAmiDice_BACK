53018b1de14ef99faa7005b2456e6675265fa072
GreenPepperCode <max.bonnin@sfr.fr>
2023-02-02 11:14:58 +0100

    add public folder to express

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-
 app.use(express.urlencoded({extended : true}));
+
+app.use('/', express.static('public'));
 
 const bodyParser = multer();
 
